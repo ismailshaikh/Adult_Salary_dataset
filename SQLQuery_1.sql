2111b9b4c5eff5354a31f12459ce927d9049b159
mohammed ismail <76427625+ismailshaikh@users.noreply.github.com>
2022-06-25 20:51:35 +0530

    Delete SQLQuery_1.sql

--- SQLQuery_1.sql
+++
@@ -1,4 +0,0 @@
--- Select rows from a Table or View '[TableOrViewName]' in schema '[dbo]'
-
-SELECT MONTH(HireDate) AS "Hire Month", COUNT(NationalIDNumber) AS 'No Of Employee' FROM AdventureWorks2019.HumanResources.Employee GROUP BY MONTH (HireDate)
-GO
