51ee44646814b2e39f18a2a7f3d2e7a3877cdf0e
mohammed ismail <76427625+ismailshaikh@users.noreply.github.com>
2022-06-25 20:51:42 +0530

    Delete SQLQuery_2.sql

--- SQLQuery_2.sql
+++
@@ -1,5 +0,0 @@
-
-SELECT DISTINCT EmployeeDepartmentHistory.DepartmentID, department.NAME, EmployeeDepartmentHistory.BusinessEntityID as no_of_emp_hire
-from [AdventureWorks2019].[HumanResources].[EmployeeDepartmentHistory]
-JOIN [AdventureWorks2019].[HumanResources].[Department]
-ON [EmployeeDepartmentHistory].[DepartmentID]=[Department].[DepartmentID]
